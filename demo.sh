cd59cdc35baa5073e5288310ecee15583b495b98
shenshen-hungry <shen.li0612@gmail.com>
2017-09-02 20:11:36 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,5 @@
 # Because the epoch setting is based on NGRAM=2, if NGRAM is changed, the number of epoch should be changed.
 
-
 NGRAM=2
 python process_data.py GoogleNews-vectors-negative300.bin
 python initialize_filter.py --ngram $NGRAM mr.p
